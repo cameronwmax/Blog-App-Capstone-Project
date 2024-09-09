0a51e7e3425a71c0b739b1dccd171bbc0bbc7785
Cameron Maxwell <cameronwmax@icloud.com>
2024-09-08 21:37:56 -0700

    Finished app

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const posts = [];
+let name;
+class Post {
+  constructor(author = name, content) {
+    this.author = author;
+    this.content = content;
+    this.date = this.#getDate();
+    this.#pushPost();
+  }
+
+  #pushPost() {
+    posts.push(this);
+  }
+
+  #getDate() {
+    const today = new Date();
+    const date = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+
+    return `${month}/${date}/${year}`;
+  }
+}
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { posts, name });
+  console.log(name);
+});
+
+app.post("/start", (req, res) => {
+  name = req.body.name;
+  res.redirect("/");
+});
+
+app.get("/addPost", (req, res) => {
+  res.render("addPost.ejs", { name });
+});
+
+app.post("/addPost", (req, res) => {
+  new Post(req.body.name, req.body.content);
+  console.log(posts);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
